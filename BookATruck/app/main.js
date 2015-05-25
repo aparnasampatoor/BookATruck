bd3d8394c2d9ad223dbb0c1569cf1e00e92976db
aparnasampatoor <aparna.netdeveloper@gmail.com>
2015-05-25 16:13:26 -0400

    Added ouath

---
+++ BookATruck/app/main.js
@@ -0,0 +1,29 @@
+﻿OAuth.initialize('pxaqfVWvv9PfOhrYPIr6H0zW2Fo');
+
+//OAuth.popup('facebook')
+//    .done(function(result) {
+//        //use result.access_token in your API request 
+//        //or use result.get|post|put|del|patch|me methods (see below)
+//        //result.get('/me')
+//        //    .done(function(response) {
+//        //        //this will display "John Doe" in the console
+//        //        console.log(response.name);
+//        //        console.log(response);
+//        //    })
+//        //    .fail(function(err) {
+//        //        //handle error with err
+//        //    });
+
+//        result.me(['firstname', 'lastname', 'email'])
+//    .done(function (response) {
+//        console.log('Firstname: ', response.firstname);
+//        console.log('Obj', response);
+//    })
+//    .fail(function (err) {
+//        //handle error with err
+//    });
+//    })
+//    .fail(function(err) {
+//        //handle error with err
+//        console.log(err);
+//    });
