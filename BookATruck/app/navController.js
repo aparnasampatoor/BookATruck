ac72161f242e44ce725199d581922f44fd1caf08
aparnasampatoor <aparna.netdeveloper@gmail.com>
2015-06-27 10:06:59 -0400

    test

---
+++ BookATruck/app/navController.js
@@ -0,0 +1,73 @@
+﻿(function() {
+    angular.module('app')
+        .controller('navController', navController);
+
+    navController.$inject = ['$location', '$http', '$scope'];
+
+    function navController($location, $http, $scope) {
+        $scope.loggedIn = loggedIn == 1 ? true : false;
+
+        OAuth.initialize('jTgRorpWj4znaClTXsJvU9jAkHs');
+
+        $scope.FacebookLogin = function () {
+            if ($scope.loggedIn == false) {
+                OAuth.popup('facebook')
+                    .done(function(result) {
+                        result.me(['firstname', 'lastname', 'email'])
+                            .done(function(response) {
+                                console.log('Firstname: ', response.firstname);
+                                var cookieVal = {
+                                    FirstName: response.firstName,
+                                    LastName: response.lastName,
+                                    Email: response.email
+                                };
+                                $scope.loggedIn = true;
+                                $.cookie('BookTruck', JSON.stringify(cookieVal));
+                                $scope.$apply();
+                            })
+                            .fail(function(err) {
+                                //handle error with err
+                            });
+                    })
+                    .fail(function(err) {
+                        //handle error with err
+                        console.log(err);
+                    });
+            }
+        }
+
+        $scope.Logout = function () {
+            $.cookie("BookTruck", null, { path: '/' });
+            $scope.loggedIn = false;
+        }
+
+        $scope.GoogleLogin = function () {
+            if ($scope.loggedIn == false) {
+                OAuth.popup('google').done(function(result) {
+                        result.me(['firstname', 'lastname', 'email'])
+                            .done(function(response) {
+                                console.log('Firstname: ', response.firstname);
+                                var cookieVal = {
+                                    FirstName: response.firstName,
+                                    LastName: response.lastName,
+                                    Email: response.email
+                                };
+                                $scope.loggedIn = true;
+                                $.cookie('BookTruck', JSON.stringify(cookieVal));
+                                $scope.$apply();
+                            })
+                            .fail(function(err) {
+                                //handle error with err
+                            });
+                    })
+                    .fail(function(err) {
+                        //handle error with err
+                        console.log(err);
+                    });
+            }
+        }
+
+    }
+
+})
+();
---
+++ BookATruck/app/searchResults/searchResultsController.js
@@ -0,0 +1,57 @@
+﻿(function () {
+    angular.module('app')
+        .controller('searchResultsController', searchResultsController);
+
+    searchResultsController.$inject = ['$location', '$http', '$modal'];
+
+    function searchResultsController($location, $http, $modal) {
+        var vm = this;
+        vm.routes = [];
+        vm.isLoading = false;
+        vm.truckTypes = [];
+        
+        vm.searchRoutes = function () {
+            vm.isLoading = true;
+            var search = $location.search();
+            $http.get('api/booktruck/SearchRoute?Source=' + search.Source + '&Destination=' + search.Destination + '&SearchDate=' + search.FromDate + '&TruckTypeId=' + vm.filter.truckType.Id + '&loadType=' + vm.filter.loadType).then(function (response) {
+                vm.routes = response.data;
+                vm.isLoading = false;
+            });
+        }
+
+        activate();
+        
+        function activate() {
+            
+            vm.filter = {
+                loadType: null,
+                volume: 0,
+                weight: 0,
+                truckType: {Id : 0 , Name : 'Select Truck Type'}
+            };
+            $http.get('api/booktruck/TruckTypes').then(function (response) {
+                vm.truckTypes = response.data;
+                vm.filter.truckType = vm.truckTypes[0] ? vm.truckTypes[0] : '';
+
+            });
+            vm.searchRoutes();
+
+        }
+
+        
+
+        vm.booktruck = function () {
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'app/searchresults/modalContent.html',
+                size: 'sm'
+
+            });
+        }
+
+        vm.truckTypeChanged = function (truckType) {
+            vm.filter.truckType = truckType;
+            vm.searchRoutes();
+        };
+    }
+})();
